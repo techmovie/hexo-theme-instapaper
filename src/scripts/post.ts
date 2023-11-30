008f4118f6d03e7ee9a578b14b6833733bf340ee
techmovie <techmovie@users.noreply.github.com>
2023-11-30 22:27:58 +0800

    fix: load img

--- src/scripts/post.ts
+++ src/scripts/post.ts
@@ -3,16 +3,24 @@ const options = {
   threshold: 0,
 };
 const observer = new IntersectionObserver((changes) => {
-  changes.forEach(change => {
-    const img = change.target;
+  changes.forEach(async change => {
+    const img = change.target as HTMLImageElement;
     const imgUrl = img.getAttribute('data-original');
     if (imgUrl && change.isIntersecting) {
-      img.setAttribute('src', imgUrl || '');
+      await loadImage(imgUrl, img);
       img.classList.toggle('img-loading');
       observer.unobserve(img);
     }
   });
 }, options);
+
+async function loadImage (url:string, elem:HTMLImageElement) {
+  return new Promise((resolve, reject) => {
+    elem.onload = () => resolve(elem);
+    elem.onerror = reject;
+    elem.src = url;
+  });
+}
 const imgList = document.querySelectorAll('.img-loading');
 Array.from(imgList).forEach((item) => {
   observer.observe(item);
